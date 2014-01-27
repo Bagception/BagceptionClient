c16d45d7ee87ce4333d844575d5c2b36db5dcf95
Bibi <bibistrobel@googlemail.com>
2014-01-27 11:19:46 +0100

    bugs

--- src/de/uniulm/bagception/client/ui/launcher/ItemListArrayAdapter.java
+++ src/de/uniulm/bagception/client/ui/launcher/ItemListArrayAdapter.java
@@ -1,6 +1,8 @@
 package de.uniulm.bagception.client.ui.launcher;
 
 import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
+import de.uniulm.bagception.client.R;
+import de.uniulm.bagception.client.items.AutoUpdateableItemView;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,12 +21,14 @@ public class ItemListArrayAdapter extends ArrayAdapter<Item> {
         View view = convertView;
         if (view == null) {
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+            view = inflater.inflate(R.layout.item_list_layout, null);
         }
 
         Item item = getItem(position);
         if (item!= null) {
-            TextView itemView = (TextView) view.findViewById(android.R.id.text1);
+            TextView itemView = (TextView) view.findViewById(R.id.itemName);
+            AutoUpdateableItemView autoItemView = (AutoUpdateableItemView) view.findViewById(R.id.itemIcon);
+            autoItemView.setItem(item);
             
             if (itemView != null) {
                 itemView.setText(item.getName());
