e85045a1e81572b7cec7ad61498148b19c8d77f2
Bibi <bibistrobel@googlemail.com>
2014-01-30 12:25:21 +0100

    views

--- src/de/uniulm/bagception/client/ui/launcher/AllItemsFragment.java
+++ src/de/uniulm/bagception/client/ui/launcher/AllItemsFragment.java
@@ -1,37 +1,43 @@
 package de.uniulm.bagception.client.ui.launcher;
 
-import de.uniulm.bagception.bluetoothclientmessengercommunication.service.BundleMessageHelper;
-import de.uniulm.bagception.bundlemessageprotocol.BundleMessage;
-import de.uniulm.bagception.bundlemessageprotocol.BundleMessage.BUNDLE_MESSAGE;
-import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
-import de.uniulm.bagception.bundlemessageprotocol.entities.administration.ItemCommand;
-import de.uniulm.bagception.client.R;
-import android.R.anim;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.webkit.WebView.FindListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
+import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageActor;
+import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageReactor;
+import de.uniulm.bagception.bluetoothclientmessengercommunication.service.BundleMessageHelper;
+import de.uniulm.bagception.bundlemessageprotocol.BundleMessage;
+import de.uniulm.bagception.bundlemessageprotocol.BundleMessage.BUNDLE_MESSAGE;
+import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommand;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommandProcessor;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommandReceiverCallbacks;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.ItemCommand;
+import de.uniulm.bagception.client.R;
 
-public class AllItemsFragment extends Fragment{
+public class AllItemsFragment extends Fragment implements BundleMessageReactor{
 	
 	ListView listView;
-	ArrayAdapter<Item> ad;
+	ItemListArrayAdapter ad;
 	String bla;
-	
+	BundleMessageActor actor;
 	public static Fragment newInstance(Context context) {
 		AllItemsFragment f = new AllItemsFragment();
 
 		return f;
 	}
 	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		actor = new BundleMessageActor(this);
+		
+	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,7 +45,83 @@ public class AllItemsFragment extends Fragment{
 		final ViewGroup root = (ViewGroup) inflater.inflate(
 				R.layout.fragment_all_items, null);
 		listView = (ListView) root.findViewById(R.id.listViewAllItems);
+		ad = new ItemListArrayAdapter(getActivity());
+		listView.setAdapter(ad);
 		return root;
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		BundleMessageHelper helper = new BundleMessageHelper(getActivity());
+		actor.register(getActivity()); //necessary?
+		helper.sendMessageSendBundle(BundleMessage.getInstance().createBundle(BUNDLE_MESSAGE.ADMINISTRATION_COMMAND, ItemCommand.list()));
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+		actor.unregister(getActivity()); //necessary?
+	}
+	
+	//BundleMessageReactor
+
+	@Override
+	public void onBundleMessageRecv(Bundle b) {
+		BUNDLE_MESSAGE msg = BundleMessage.getInstance().getBundleMessageType(b);
+		switch (msg){
+		case ADMINISTRATION_COMMAND:
+			AdministrationCommand<?> a_cmd = AdministrationCommand.fromJSONObject(BundleMessage.getInstance().extractObject(b));
+			AdministrationCommandProcessor adminCommandProcessor = new AdministrationCommandProcessor(){
+				@Override
+				public void onItemList(AdministrationCommand<Item> i) {
+					//item list
+					Item[] theItemsWeWantToDisplay = i.getPayloadObjects();
+					ad.addAll(theItemsWeWantToDisplay);
+				}
+			};
+			a_cmd.accept(adminCommandProcessor);
+
+			break;
+			
+			default: break;
+		}
+			}
+
+	@Override
+	public void onBundleMessageSend(Bundle b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onResponseMessage(Bundle b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onResponseAnswerMessage(Bundle b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusMessage(Bundle b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCommandMessage(Bundle b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(Exception e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
