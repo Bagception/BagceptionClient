df098effc5f1284a83a9d60b2af43a9999a7e544
Bibi <bibistrobel@googlemail.com>
2014-02-11 13:28:05 +0100

    overlays

--- src/de/uniulm/bagception/client/ui/launcher/CreateNewPlaceFragment.java
+++ src/de/uniulm/bagception/client/ui/launcher/CreateNewPlaceFragment.java
@@ -1,5 +1,6 @@
 package de.uniulm.bagception.client.ui.launcher;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
@@ -8,8 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import de.uniulm.bagception.bluetoothclientmessengercommunication.service.BundleMessageHelper;
 import de.uniulm.bagception.bundlemessageprotocol.BundleMessage;
 import de.uniulm.bagception.bundlemessageprotocol.BundleMessage.BUNDLE_MESSAGE;
@@ -26,6 +30,10 @@ public class CreateNewPlaceFragment extends Fragment {
 	Button send;
 	ShowMap showMap;
 	Button cancel;
+	Button bt;
+	Button wlan;
+	ListView btListView;
+	ListAdapter adapter;
 
 	public static Fragment newInstance(Context context) {
 		CreateNewPlaceFragment f = new CreateNewPlaceFragment();
@@ -36,18 +44,40 @@ public class CreateNewPlaceFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		ViewGroup root = (ViewGroup) inflater.inflate(
+		final ViewGroup root = (ViewGroup) inflater.inflate(
 				R.layout.fragment_create_new_place, null);
 		editName = (EditText) root.findViewById(R.id.editName);
 		send = (Button) root.findViewById(R.id.send);
 		cancel = (Button) root.findViewById(R.id.cancelPlace);
+		bt = (Button) root.findViewById(R.id.btButton);
+		wlan = (Button) root.findViewById(R.id.wlanButton);
 		showMap = new ShowMap();
+		
+		final String[] test = {"1", "2"};
+
+		bt.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String names[] = {"1", "2"};
+				// TODO Auto-generated method stub
+				AlertDialog.Builder dialogAlert = new AlertDialog.Builder(
+						getActivity());
+				dialogAlert.setTitle("BT");
+				ListView lv = (ListView) root.findViewById(R.id.btListview);
+				ListAdapter ladapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, test);
+				lv.setAdapter(ladapter);
+				dialogAlert.create().show();
+			}
+		});
+
 		send.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 
-				Location location = new Location(editName.getText().toString(), "testMAC");
+				Location location = new Location(editName.getText().toString(),
+						"testMAC");
 
 				BundleMessageHelper helper = new BundleMessageHelper(
 						getActivity());
@@ -59,9 +89,9 @@ public class CreateNewPlaceFragment extends Fragment {
 				startActivity(intent);
 			}
 		});
-		
+
 		cancel.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
