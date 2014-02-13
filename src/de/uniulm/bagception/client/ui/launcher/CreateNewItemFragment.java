9b2e4d943808157e28ea08d53bd2746d932a2bf4
Bibi <bibistrobel@googlemail.com>
2014-02-13 14:26:18 +0100

    create items

--- src/de/uniulm/bagception/client/ui/launcher/CreateNewItemFragment.java
+++ src/de/uniulm/bagception/client/ui/launcher/CreateNewItemFragment.java
@@ -1,5 +1,9 @@
 package de.uniulm.bagception.client.ui.launcher;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
@@ -12,21 +16,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageActor;
+import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageReactor;
 import de.uniulm.bagception.bluetoothclientmessengercommunication.service.BundleMessageHelper;
 import de.uniulm.bagception.bundlemessageprotocol.BundleMessage;
 import de.uniulm.bagception.bundlemessageprotocol.BundleMessage.BUNDLE_MESSAGE;
+import de.uniulm.bagception.bundlemessageprotocol.entities.Category;
 import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
+import de.uniulm.bagception.bundlemessageprotocol.entities.ItemAttribute;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommand;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommandProcessor;
+import de.uniulm.bagception.bundlemessageprotocol.entities.administration.CategoryCommand;
 import de.uniulm.bagception.bundlemessageprotocol.entities.administration.ItemCommand;
 import de.uniulm.bagception.client.R;
 
-public class CreateNewItemFragment extends Fragment {
+public class CreateNewItemFragment extends Fragment implements
+		BundleMessageReactor {
 
+	Category categoryForActivity;
 	EditText editName;
 	Button send;
 	Button cancel;
@@ -39,8 +57,24 @@ public class CreateNewItemFragment extends Fragment {
 	ToggleButton dark;
 	Spinner spinner;
 	ImageView iv;
+	CheckBox always;
+	CheckBox independet;
+	String warmOn;
+	String coldOn;
+	String rainyOn;
+	String sunnyOn;
+	String lightOn;
+	String darkOn;
+	String temperature;
+	String weather;
+	String lightness;
+	boolean alwaysChecked;
+	boolean independetChecked;
+	BundleMessageActor bmActor;
 
 	private String tagId;
+	private ArrayList<String> tagIDs;
+
 	static Fragment newInstance(Context context) {
 		CreateNewItemFragment f = new CreateNewItemFragment();
 
@@ -63,32 +97,22 @@ public class CreateNewItemFragment extends Fragment {
 		rainy = (ToggleButton) root.findViewById(R.id.rainButton);
 		light = (ToggleButton) root.findViewById(R.id.lightButton);
 		dark = (ToggleButton) root.findViewById(R.id.darkButton);
+		always = (CheckBox) root.findViewById(R.id.always);
+		independet = (CheckBox) root.findViewById(R.id.independent);
 
+		bmActor = new BundleMessageActor(this);
 		addCategory.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				String names[] = { "1", "2" };
-				// TODO Auto-generated method stub
-				AlertDialog.Builder btAlert = new AlertDialog.Builder(
-						getActivity());
 
-				btAlert.setTitle("Kategorie");
-
-				final CharSequence[] test = { "1", "2" };
-				btAlert.setItems(test, new DialogInterface.OnClickListener() {
-
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						// TODO Auto-generated method stub
-
-					}
-				});
-				btAlert.create().show();
+				new BundleMessageHelper(getActivity())
+						.sendMessageSendBundle(BundleMessage.getInstance()
+								.createBundle(
+										BUNDLE_MESSAGE.ADMINISTRATION_COMMAND,
+										CategoryCommand.list()));
 			}
 		});
-		
-
 
 		if (getArguments() == null) {
 			Toast.makeText(getActivity(), "Arguments null", Toast.LENGTH_SHORT);
@@ -106,103 +130,120 @@ public class CreateNewItemFragment extends Fragment {
 				tagId = getArguments().getString("TAGID");
 
 			}
-		
-		Toast.makeText(getActivity(), "load item with id " + id,
-				Toast.LENGTH_SHORT).show();
+
+			Toast.makeText(getActivity(), "load item with id " + id,
+					Toast.LENGTH_SHORT).show();
 		}
-		warm.setOnClickListener(new OnClickListener() {
+
+		warm.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = warm.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					warmOn = "warm";
 				} else {
-					// Disable vibrate
+					warmOn = null;
 				}
-
 			}
 		});
 
-		cold.setOnClickListener(new OnClickListener() {
+		cold.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = cold.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					coldOn = "cold";
 				} else {
-					// Disable vibrate
+					coldOn = null;
 				}
-
 			}
 		});
 
-		sunny.setOnClickListener(new OnClickListener() {
+		rainy.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = sunny.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					rainyOn = "rainy";
 				} else {
-					// Disable vibrate
+					rainyOn = null;
 				}
-
 			}
 		});
 
-		rainy.setOnClickListener(new OnClickListener() {
+		sunny.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = rainy.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					sunnyOn = "sunny";
 				} else {
-					// Disable vibrate
+					sunnyOn = null;
 				}
-
 			}
 		});
 
-		light.setOnClickListener(new OnClickListener() {
+		light.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = light.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					lightOn = "light";
 				} else {
-					// Disable vibrate
+					lightOn = null;
 				}
-
 			}
 		});
 
-		dark.setOnClickListener(new OnClickListener() {
+		dark.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
 				// TODO Auto-generated method stub
-				boolean on = dark.isChecked();
-
-				if (on) {
-					// Enable vibrate
+				if (isChecked) {
+					darkOn = "cold";
 				} else {
-					// Disable vibrate
+					darkOn = null;
 				}
+			}
+		});
 
+		always.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					alwaysChecked = true;
+				} else {
+					alwaysChecked = false;
+				}
+			}
+		});
+
+		independet.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					independetChecked = true;
+				} else {
+					independetChecked = false;
+				}
 			}
 		});
 
@@ -211,23 +252,56 @@ public class CreateNewItemFragment extends Fragment {
 			@Override
 			public void onClick(View arg0) {
 				Item item;
-				if (tagId != null){
-					item = new Item(editName.getText().toString(),tagId);
+				if (tagId != null) {
 
-				}else{
-					item = new Item(editName.getText().toString());
+					tagIDs.add(tagId);
 
 				}
-				
-				
-				item.setImage(((MainGUI) getActivity()).currentPicturetaken);
 
+				if (warmOn == null && coldOn == null) {
+					temperature = null;
+				} else if (warmOn != null && coldOn == null) {
+					temperature = warmOn;
+				} else if (warmOn == null && coldOn != null) {
+					temperature = coldOn;
+				} else if (warmOn != null && coldOn != null) {
+					temperature = "Both selected";
+				}
+
+				if (rainyOn == null && sunnyOn == null) {
+					weather = null;
+				} else if (rainyOn != null && sunnyOn == null) {
+					weather = rainyOn;
+				} else if (rainyOn == null && sunnyOn != null) {
+					weather = sunnyOn;
+				} else if (rainyOn != null && sunnyOn != null) {
+					weather = "Both selected";
+				}
+
+				if (lightOn == null && darkOn == null) {
+					lightness = null;
+				} else if (lightOn != null && darkOn == null) {
+					lightness = lightOn;
+				} else if (lightOn == null && darkOn != null) {
+					lightness = darkOn;
+				} else if (lightOn != null && darkOn != null) {
+					lightness = "Both selected";
+				}
+
+				ItemAttribute attributes = new ItemAttribute(temperature,
+						weather, lightness);
+				item = new Item(-1, editName.getText().toString(),
+						categoryForActivity, alwaysChecked, independetChecked,
+						attributes, tagIDs);
+
+				item.setImage(((MainGUI) getActivity()).currentPicturetaken);
+				
 				BundleMessageHelper helper = new BundleMessageHelper(
 						getActivity());
 				helper.sendMessageSendBundle(BundleMessage.getInstance()
 						.createBundle(BUNDLE_MESSAGE.ADMINISTRATION_COMMAND,
 								ItemCommand.add(item)));
-
+				Log.d("TEST", item.toString());
 				Intent intent = new Intent(getActivity(), MainGUI.class);
 				startActivity(intent);
 			}
@@ -245,6 +319,106 @@ public class CreateNewItemFragment extends Fragment {
 		});
 
 		return root;
+	}
+
+	@Override
+	public void onBundleMessageRecv(Bundle b) {
+		// TODO Auto-generated method stub
+		switch (BundleMessage.getInstance().getBundleMessageType(b)) {
+		case ADMINISTRATION_COMMAND: {
+			AdministrationCommandProcessor p = new AdministrationCommandProcessor() {
+
+				@Override
+				public void onCategoryList(
+						de.uniulm.bagception.bundlemessageprotocol.entities.administration.AdministrationCommand<de.uniulm.bagception.bundlemessageprotocol.entities.Category> i) {
+					// TODO Auto-generated method stub
+					final Category[] categories = i.getPayloadObjects();
+					final HashSet<Integer> checkedCategories = new HashSet<Integer>();
+					final String[] categoryStrings = new String[categories.length];
+					for (int iter = 0; iter < categoryStrings.length; iter++) {
+						categoryStrings[iter] = categories[iter].getName();
+					}
+
+					AlertDialog.Builder categoryAlert = new AlertDialog.Builder(
+							getActivity());
+					categoryAlert.setTitle("Items zur Activity hinzufügen");
+
+					categoryAlert.setItems(categoryStrings,
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog,
+										int which) {
+									// TODO Auto-generated method stub
+									categoryForActivity = new Category(
+											categoryStrings[which]);
+								}
+							});
+					categoryAlert.create().show();
+
+				}
+
+			};
+			AdministrationCommand.fromJSONObject(
+					BundleMessage.getInstance().extractObject(b)).accept(p);
+
+		}
+
+		default:
+			break;
+
+		}
+
+	}
+
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		bmActor.register(getActivity());
+		super.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		// TODO Auto-generated method stub
+		bmActor.unregister(getActivity());
+		super.onPause();
+	}
+
+	@Override
+	public void onBundleMessageSend(Bundle b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onResponseMessage(Bundle b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onResponseAnswerMessage(Bundle b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusMessage(Bundle b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onCommandMessage(Bundle b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onError(Exception e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
