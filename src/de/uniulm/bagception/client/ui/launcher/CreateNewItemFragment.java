2a6878cc5394b83cdcf08d6849853814c6ff7bdf
Philipp Hock <philipp.hock@uni-ulm.de>
2014-02-13 16:28:45 +0100

    fixed bibi bug

--- src/de/uniulm/bagception/client/ui/launcher/CreateNewItemFragment.java
+++ src/de/uniulm/bagception/client/ui/launcher/CreateNewItemFragment.java
@@ -2,22 +2,18 @@ package de.uniulm.bagception.client.ui.launcher;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
