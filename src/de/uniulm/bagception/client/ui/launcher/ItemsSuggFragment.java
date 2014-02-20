892b5d88f12eba90cc450cb484913c961b4bbd01
ms122 <michael.schiller@uni-ulm.de>
2014-02-20 23:18:25 +0100

    Finish Suggestion GUI

--- src/de/uniulm/bagception/client/ui/launcher/ItemsSuggFragment.java
+++ src/de/uniulm/bagception/client/ui/launcher/ItemsSuggFragment.java
@@ -1,20 +1,25 @@
 package de.uniulm.bagception.client.ui.launcher;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.Toast;
 import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageActor;
 import de.uniulm.bagception.bluetoothclientmessengercommunication.actor.BundleMessageReactor;
 import de.uniulm.bagception.bundlemessageprotocol.entities.ContainerStateUpdate;
 import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
 import de.uniulm.bagception.client.R;
 
-public class ItemsSuggFragment extends OverviewTabFragment implements BundleMessageReactor{
+public class ItemsSuggFragment extends OverviewTabFragment implements BundleMessageReactor, OnChildClickListener{
 
 	private OverviewFragment fragment;
 	private ListView itemsRedundantView;
@@ -31,13 +36,47 @@ public class ItemsSuggFragment extends OverviewTabFragment implements BundleMess
 		ViewGroup root = (ViewGroup) inflater.inflate(
 				R.layout.fragment_items_sugg, null);
 
-
-		itemsRedundantView = (ListView) root.findViewById(R.id.itemsSugg);
-		arrayAdapter = new ItemListArrayAdapter(getActivity());//TODO kein listadapter
-		//arrayAdapter.addAll(suggestionToReplace);
-		itemsRedundantView.setAdapter(arrayAdapter);
+		
+		ExpandableListView expandbleLis = (ExpandableListView) root.findViewById(R.id.itemSugg);
+		expandbleLis.setDividerHeight(2);
+		expandbleLis.setGroupIndicator(null);
+		expandbleLis.setClickable(true);
+		
+		setGroupData();
+		setChildGroupData();	
+		
+		SuggestionListAdapter suggAdapter = new SuggestionListAdapter(groupItem, childItem);
+		suggAdapter.setInflater((LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE), getActivity());
+		expandbleLis.setAdapter(suggAdapter);
 		
 		return root;
+
+	}
+	
+	ArrayList<String> groupItem = new ArrayList<String>();
+	ArrayList<Object> childItem = new ArrayList<Object>();
+	
+	
+	public void setGroupData(){
+		groupItem.add("Kurze Hose");
+		groupItem.add("T-Shirt");
+	}
+	
+	public void setChildGroupData(){
+		
+		// Data for "Kurze Hose"
+		ArrayList<String> child = new ArrayList<String>();
+		child.add("Lange Jogginghose");
+		child.add("Lange Jeans");
+		childItem.add(child);
+		
+		
+		// Data for "T-Shirt"
+		child = new ArrayList<String>();
+		child.add("Strickpulli");
+		child.add("Pullover");
+		childItem.add(child);
+		
 	}
 
 	@Override
@@ -99,6 +138,14 @@ public class ItemsSuggFragment extends OverviewTabFragment implements BundleMess
 	protected List<Item> getCorrespondingItemList(ContainerStateUpdate update) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+			int childPosition, long id) {
+		
+		Toast.makeText(getActivity().getApplicationContext(), "Clicked On Child", Toast.LENGTH_SHORT).show();
+		return true;
 	}
 
 
